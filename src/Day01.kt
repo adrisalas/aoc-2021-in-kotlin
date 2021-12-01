5561a3e81359e49985ba3998d9477e8c2ea85b8e
Adrian Salas <45205815+adrisalas@users.noreply.github.com>
2021-12-01 22:36:12 +0100

    Day 1 completed!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { string -> string.toInt() }
+        var count = 0
+        var previous = numbers[0]
+
+        for (number in numbers) {
+            if (number > previous) {
+                count++
+            }
+            previous = number
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { string -> string.toInt() }
+        val window = mutableListOf(numbers[0], numbers[1], numbers[3])
+        var previous = window.reduce { acc, i -> i + acc }
+        var count = 0
+
+        for (i in 3 until numbers.size) {
+            window.removeAt(0)
+            window.add(numbers[i])
+            val value = window.reduce { acc, n -> n + acc }
+            if (value > previous) {
+                count++
+            }
+            previous = value
+        }
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
